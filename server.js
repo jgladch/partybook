67e8af66c9f46a6b90c7f8c1bd860195ff840d34
jgladch <jeffgladchun@gmail.com>
2014-10-09 21:45:25 -0700

    Added Routes, parsing forms, and styling

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-// app.use(express.bodyParser());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
@@ -13,3 +13,7 @@ var port = process.env.PORT || 3000;
 app.listen(port);
 
 console.log('Server now listening on port ' + port);
+
+app.post('/', function(req, res){
+  console.log(req.body.email);
+});
